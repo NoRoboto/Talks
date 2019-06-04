030665bf66d5af7d1fb6f81d5c6b0a2612a90d2a
daniel.diaz <daniel.diaz@globant.com>
2019-06-03 23:24:44 -0500

    Add detox actions example

--- rn-detox/detox_test/e2e/firstTest.spec.js
+++ rn-detox/detox_test/e2e/firstTest.spec.js
@@ -1,4 +1,6 @@
-describe('Example', () => {
+const { takeScreenshot } = require('./helper');
+
+describe('Example matchers', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
   });
@@ -7,13 +9,28 @@ describe('Example', () => {
     await expect(element(by.id('view-home'))).toBeVisible();
   });
 
-  it('should show hello screen after tap', async () => {
-    await element(by.id('home-main-btn')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
+  it('should select dummy_btn ui element using text matcher', async () => {
+    await expect(element(by.text('Detox TEST'))).toBeVisible();
   });
 
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
+  it('should select dummy_btn ui element using label matcher', async () => {
+    await expect(element(by.label('dummy_btn'))).toBeVisible();
+  });
+});
+
+
+describe('Example actions', () => {
+  beforeEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should show the hidden ui element only if user type 3 or more string length on text input', async () => {
+    await element(by.id('home-primary-text-input')).typeText('passcode');
+    await expect(element(by.id('home-hiden-text'))).toExist();
+  });
+
+  it('should remove the ui element if the text input is empty', async () => {
+    await element(by.id('home-primary-text-input')).clearText();
+    await expect(element(by.id('home-hiden-text'))).toNotExist();
   });
 });
